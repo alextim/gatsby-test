39e88680dca2036dce53970bd49c1294d6975068
alex tim <buildmnt@gmail.com>
2020-03-20 16:35:45 +0200

    NavBar v1

--- src/components/Header/NavBar/index.js
+++ src/components/Header/NavBar/index.js
@@ -1,27 +1,24 @@
 import React from 'react'
+import { Flex } from '@chakra-ui/core'
 
 import Brand from './Brand'
 import Burger from './Burger'
 import Menu from './Menu'
-
 import './navbar.scss'
 
+const Navbar2 = () =>
+  <Flex as="nav" 
+    role="navigation" aria-label="main navigation"
+    flexWrap="wrap"
+    justifyContent="space-between"
+    alignItems="center"
+    bg="#09c"
+    >
 
+    <Brand />
+    <Burger />
+    <Menu />
+  </Flex>
 
-
-
-const Navbar2 = () => {
-  return (
-    
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-
-      <Brand />
-      <Burger />
-      <Menu />
-    </nav>
-  )
-
-
-}
 
 export default Navbar2
