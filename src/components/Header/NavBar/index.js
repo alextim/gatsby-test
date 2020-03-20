7885795a2b356b63fe920f4cbd12869090d1bfff
alex tim <buildmnt@gmail.com>
2020-03-20 17:33:48 +0200

    navbar click

--- src/components/Header/NavBar/index.js
+++ src/components/Header/NavBar/index.js
@@ -2,23 +2,106 @@ import React from 'react'
 import { Flex } from '@chakra-ui/core'
 
 import Brand from './Brand'
-import Burger from './Burger'
-import Menu from './Menu'
+import { mainMenuItems } from './mainMenuItems'
 import './navbar.scss'
 
-const Navbar2 = () =>
-  <Flex as="nav" 
-    role="navigation" aria-label="main navigation"
-    flexWrap="wrap"
-    justifyContent="space-between"
-    alignItems="center"
-    bg="#09c"
-    >
+/*
+https://jonsuh.com/hamburgers/
+*/
 
-    <Brand />
-    <Burger />
-    <Menu />
-  </Flex>
+
+let id = 0
+
+const getId = () => {
+  id = id + 1
+  return 'sm' + id.toString()
+}
+
+const NavbarItem = ({title, url}) => (
+  <li><a href={url}>{title}</a></li>
+)
+
+const NavbarDropdown = ({title, id, isTopLevel, items}) => (
+  <li>
+    <a>
+      <span>{title}</span>
+      <span className="drop-icon">{ isTopLevel ? '▾' : '▸' }</span>
+      <label title="Toggle Drop-down" className="drop-icon" htmlFor={id} />
+    </a>
+    <input type="checkbox" id={id}/>
+    <ul className="sub-menu">
+      {
+        items.map( (item, i) => {
+          if (item.hasOwnProperty('children')) {
+            return (<NavbarDropdown key={i} title={item.title} id={getId()} isTopLevel={false} items={item.children}/>)
+          } else {
+            return (<NavbarItem key={i} title={item.title} url={item.url}/>)
+          }
+        })
+      }
+    </ul>
+  </li>
+)
+
+
+
+
+const Navbar2 = () => {
+  const [isActive, setisActive] = React.useState(false)
+
+  const Burger = () => {
+  
+    return (
+      <button 
+        id="toggle-menu" 
+        type="button" 
+        aria-label="Menu" aria-controls="navigation"
+        onClick={() => {
+          setisActive(!isActive);
+        }}
+        role="button"
+        className={`hamburger hamburger--spin ${isActive ? "is-active" : ""}`}
+      >
+        <span className="hamburger-box">
+          <span className="hamburger-inner"></span>
+        </span>
+      </button>
+    )
+  }
+  
+
+  const Menu = () => {
+    return (
+      <ul id="navbar-menu" className={`navbar-menu ${isActive ? "is-active" : ""}`}> 
+      {
+        mainMenuItems.map ( (item) => {
+          if (item.hasOwnProperty('children')) {
+            return (<NavbarDropdown title={item.title}  id={getId()} isTopLevel={true} items={item.children} />)
+          } else {
+            return (<NavbarItem title={item.title} url={item.url}/>)
+          }
+        } )
+      }
+        </ul>
+      )
+  }
+  
+
+  return (
+    <Flex as="nav" 
+      role="navigation" aria-label="main navigation"
+      flexWrap="wrap"
+      justifyContent="space-between"
+      alignItems="center"
+      bg="#09c"
+      >
+
+      <Brand />
+      <Burger />
+      <Menu />
+    </Flex>
+    )
+}
 
 
 export default Navbar2
