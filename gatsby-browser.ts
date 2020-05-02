d9add4c2d201b0bfe0a1a842f06fa5bd91610b22
alex tim <buildmnt@gmail.com>
2020-05-02 22:32:34 +0300

    Slider

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,3 +1,3 @@
 import Provider from './src/state/provider';
-console.log('BROWSER----------------------------');
+
 export const wrapRootElement = Provider;
--- src/components/home/Slider.tsx
+++ src/components/home/Slider.tsx
@@ -1,5 +1,50 @@
+import React from 'react';
+import Img from 'gatsby-image';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+import useDefaultBannerImage from '../../helpers/hooks/useDefaultBannerImage';
+
+import { ContainerFullWidth } from '../Container';
+
+const carouselProps = {
+  showArrows: true,
+  showStatus: false,
+  showIndicators: true,
+  infiniteLoop: true,
+  showThumbs: false,
+  useKeyboardArrows: false,
+  autoPlay: true,
+  stopOnHover: true,
+  swipeable: true,
+  dynamicHeight: false,
+  emulateTouch: true,
+  thumbWidth: 100,
+  selectedItem: 0,
+  interval: 4000,
+  transitionTime: 300,
+  swipeScrollTolerance: 5,
+};
 const Slider = () => {
-  return <div>Slider!</div>;
+  const fluid = useDefaultBannerImage();
+  return (
+    <ContainerFullWidth>
+      <Carousel {...carouselProps}>
+        <div>
+          <Img fluid={fluid} />
+          <p className="legend">Legend 1</p>
+        </div>
+        <div>
+          <img src="https://picsum.photos/1920/800" />
+          <p className="legend">Legend 2</p>
+        </div>
+        <div>
+          <img src="https://picsum.photos/1920/800" />
+          <p className="legend">Legend 3</p>
+        </div>
+      </Carousel>
+    </ContainerFullWidth>
+  );
 };
 
 export default Slider;
